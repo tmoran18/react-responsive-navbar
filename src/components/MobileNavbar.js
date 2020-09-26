f10b5704cac21f72f1a647c4468179305bc9ccbe
Tim Moran <moran.tim01@gmail.com>
2020-09-27 08:27:04 +1000

    added styles to Mobile Nav Added onclick function and state to open/close

--- src/components/MobileNavbar.js
+++ src/components/MobileNavbar.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles.css";
 
 const MobileNavbar = () => {
+  const [mobileNavIsOpen, setMobileNavisOpen] = useState("false");
+
+  // Toggle the Mobile Nav open / close
+  const handleMobileNavClick = () => {
+    mobileNavIsOpen ? setMobileNavisOpen(false) : setMobileNavisOpen(true);
+  };
+
   return (
     <nav className="mobileNav_container">
       <img src="./logo.svg" width="50" alt="logo" />
@@ -9,9 +16,14 @@ const MobileNavbar = () => {
         className="menu_icon"
         src="./menu_icon.svg"
         alt="hamburger menu icon"
+        onClick={handleMobileNavClick}
       />
-      <div>
-        <ul className="mobileNav_menu_list">
+      <div className="mobileNav_menu_container">
+        <ul
+          className={`${
+            mobileNavIsOpen ? "mobileNav_open" : "mobileNav_closed"
+          } mobileNav_menu_list`}
+        >
           <li>Home</li>
           <li>About</li>
           <li>Work</li>
